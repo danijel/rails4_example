c3dfaca075bcb14ac98c19441b8d48a4d2116fb6
Danijel Bjelajac <danijel.bjelajac@gmail.com>
2014-09-22 05:52:58 +0200

    using original filename in uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -45,8 +45,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    "avatar.jpeg" if original_filename
-  end
+  # def filename
+  #   "avatar.jpeg" if original_filename
+  # end
 
 end
