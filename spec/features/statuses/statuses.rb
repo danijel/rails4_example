f5a7441fd60162f9650fced7f5525888bcff5487
Danijel Bjelajac <danijel.bjelajac@gmail.com>
2014-09-22 02:23:07 +0200

    status controller and index page

---
+++ spec/features/statuses/statuses.rb
@@ -0,0 +1,25 @@
+include Warden::Test::Helpers
+Warden.test_mode!
+
+feature 'Statuses crud' do
+
+  after(:each) do
+    Warden.test_reset!
+  end
+
+  given (:user) { FactoryGirl.create :user }
+
+  # Scenario: Statuses listed on status index page
+  #   Given I am signed in
+  #   When I visit the statuses index page
+  #   Then I see all statuses
+  scenario 'user sees all statuses' do
+    s1 = FactoryGirl.create(:status, user: user)
+    s2 = FactoryGirl.create(:status, user: user)
+    login_as(user, scope: :user)
+    visit statuses_path
+    expect(page).to have_content s1.status
+    expect(page).to have_content s2.status
+  end
+
+end
